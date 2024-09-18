58224df9a393a85406737a90cfa5c82f3a0438a4
hcollingwood <hannah.collingwood@telespazio.com>
2024-09-18 09:44:37 +0100

    EODHP-633 - update __main__ to rfail if tests don't pass

--- resource_catalogue_tests/__main__.py
+++ resource_catalogue_tests/__main__.py
@@ -5,10 +5,13 @@ import pytest
 
 def main():
     try:
-        pytest.main(["."])
+        results = pytest.main(["."])
     except Exception as e:
         logging.info(e)
         raise
+
+    if results != 0:
+        raise Exception("Not all tests have passed")
 
 
 if __name__ == "__main__":
